02dec96173529b0f2f7b0c6449aa51a52df270d6
Arnaud Muller <32609359+Nosudrum@users.noreply.github.com>
2024-08-30 16:56:16 +0200

    Update automation.py

--- src/automation.py
+++ src/automation.py
@@ -1,11 +1,12 @@
 from plotsCodes.all_europe import plot_all_europe
 from plotsCodes.all_europe_portrait import plot_all_europe_portrait
+from plotsCodes.distance_per_duration_stacked import plot_distance_per_duration_stacked
 from plotsCodes.germany_2023 import plot_germany_2023
 from plotsCodes.milano_iac_2024 import plot_milano_iac_2024
+from plotsCodes.number_per_duration_stacked import plot_number_per_duration_stacked
+from plotsCodes.october_lausanne_2024 import plot_october_lausanne_2024
 from plotsCodes.scandinavia_2024 import plot_scandinavia_2024
 from plotsCodes.scandinavia_2024_portrait import plot_scandinavia_2024_portrait
-from plotsCodes.number_per_duration_stacked import plot_number_per_duration_stacked
-from plotsCodes.distance_per_duration_stacked import plot_distance_per_duration_stacked
 from processing import process_data
 from utils import get_mapbox_secrets
 
@@ -24,6 +25,7 @@ if __name__ == "__main__":
     plot_scandinavia_2024(trips, mapbox_style_id=MAPBOX_STYLE_ID, mapbox_style_token=MAPBOX_STYLE_TOKEN)
     plot_scandinavia_2024_portrait(trips, mapbox_style_id=MAPBOX_STYLE_ID, mapbox_style_token=MAPBOX_STYLE_TOKEN)
     plot_milano_iac_2024(trips, mapbox_style_id=MAPBOX_STYLE_ID, mapbox_style_token=MAPBOX_STYLE_TOKEN)
+    plot_october_lausanne_2024(trips, mapbox_style_id=MAPBOX_STYLE_ID, mapbox_style_token=MAPBOX_STYLE_TOKEN)
     plot_number_per_duration_stacked(trips)
     plot_distance_per_duration_stacked(trips)
 
